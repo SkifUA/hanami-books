8268441638c62abd11133cd938cad18feb5b4794
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-11-10 16:53:44 +0200

    Added action Create Book

---
+++ apps/web/controllers/books/create.rb
@@ -0,0 +1,23 @@
+module Web::Controllers::Books
+  class Create
+    include Web::Action
+
+    params do
+      required(:book).schema do
+        required(:title).filled(:str?)
+        required(:author).filled(:str?)
+        required(:summary)
+      end
+    end
+
+    def call(params)
+      if params.valid?
+        BookRepository.new.create(params[:book])
+
+        redirect_to routes.books_path
+      else
+        self.status = 422
+      end
+    end
+  end
+end
